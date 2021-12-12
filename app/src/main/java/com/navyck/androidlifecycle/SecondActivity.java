18391cd27991c2daea60f498b97ba9f6bc6c8175
navyCK <ckck81026@gmail.com>
2021-12-12 19:19:25 +0900

    [master] 두번째 액티비티

---
+++ app/src/main/java/com/navyck/androidlifecycle/SecondActivity.java
@@ -0,0 +1,56 @@
+package com.navyck.androidlifecycle;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    private static final String TAG = "LifeCycle2";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate()");
+        setContentView(R.layout.activity_second);
+        Button mFinishButton = findViewById(R.id.finishActivity);
+        mFinishButton.setOnClickListener(v -> finish());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
+    }
+
+}
